98ce1051b7ef85760a9fa2535d8a4a423ec6422d
Mohit Sharma <mohit.vsht@gmail.com>
2023-03-09 02:05:01 +0530

    modal functionality and realtime info changing feature done

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './static/css/form.css'
 import db from '../firebase/firebase';
 import { ref, set } from 'firebase/database';
@@ -20,18 +20,28 @@ const Form = (props) => {
         let click = document.getElementById('cat_click_input').value;
         let img_cat = document.getElementById('cat_img_input').files[0];
 
+        // Comparing submit data with active cat
+        // changing clicks
+        if (props.actv_cat['click'] != click) {
+            set(ref(db, 'cats_data/' + name + '/click/'), click);
+            props.setActvCat({
+                'name': name,
+                'click': click,
+                'img_cat': props.actv_cat['img_cat'],
+            })
+        }
+
+        // changing Image
         // converting img to bas64 String
         const reader = new FileReader();
         reader.onloadend = () => {
-            let string_temp = reader.result;
+            let string_temp = reader.result.toString();
             let string_array = string_temp.split('/')
 
-            //Setting data in firebase
-            set(ref(db, 'cats_data/' + name + '/'), {
-                'name': name,
-                'click': click,
-                'img_cat': string_array,
-            });
+            if (props.actv_cat['img_cat'] != string_array) {
+                //Setting data in firebase
+                set(ref(db, 'cats_data/' + name + '/img_cat/'), string_array);
+            }
             props.setActvCat({
                 'name': name,
                 'click': click,
@@ -39,9 +49,43 @@ const Form = (props) => {
             })
         }
         reader.readAsDataURL(img_cat);
+
+
+
+        // set(ref(db, 'cats_data/' + name + '/'), {
+        //     'name': name,
+        //     'click': click,
+        //     'img_cat': string_array,
+        // })
+
+        // props.setActvCat({
+        //     'name': name,
+        //     'click': click,
+        //     'img_cat': string_array,
+        // })
+
+        //     // converting img to bas64 String
+        //     const reader = new FileReader();
+        // reader.onloadend = () => {
+        //     let string_temp = reader.result;
+        //     let string_array = string_temp.split('/')
+
+        //     //Setting data in firebase
+        //     set(ref(db, 'cats_data/' + name + '/'), {
+        //         'name': name,
+        //         'click': click,
+        //         'img_cat': string_array,
+        //     });
+        //     props.setActvCat({
+        //         'name': name,
+        //         'click': click,
+        //         'img_cat': string_array,
+        //     })
+        // }
+        // reader.readAsDataURL(img_cat);
+
         let form = document.getElementById('cat_form');
         form.reset();
-
     }
 
     // Reset form function
@@ -49,6 +93,86 @@ const Form = (props) => {
         e.preventDefault();
         let form = document.getElementById('cat_form');
         form.reset();
+    }
+
+    // changing clicks on update
+    useEffect(() => {
+        let elem = document.getElementById('cat_click_input');
+        elem.value = elem.defaultValue;
+        let elem_file= document.getElementById('cat_img_input');
+        elem_file.value = '';
+        let modal_name= document.getElementById('cat_name_input_modal')
+        modal_name.value = modal_name.defaultValue;
+        let modal_click= document.getElementById('cat_click_input_modal')
+        modal_click.value = modal_click.defaultValue;
+        let modal_file= document.getElementById('cat_img_input_modal')
+        modal_file.value = '';
+    }, [props.actv_cat]);
+
+    // Modal form editing
+    function modaleditFunction(e) {
+        e.preventDefault();
+        // getting data
+        let name = document.getElementById('cat_name_input_modal').value;
+        if (props.actv_cat['name'] == name) {
+            let click = document.getElementById('cat_click_input_modal').value;
+            let img_cat = document.getElementById('cat_img_input_modal').files[0];
+
+            // Comparing submit data with active cat
+            // changing clicks
+            if (props.actv_cat['click'] != click) {
+                set(ref(db, 'cats_data/' + name + '/click/'), click);
+                props.setActvCat({
+                    'name': name,
+                    'click': click,
+                    'img_cat': props.actv_cat['img_cat'],
+                })
+            }
+
+            // changing Image
+            // converting img to bas64 String
+            try {
+                const reader = new FileReader();
+                reader.onloadend = () => {
+                    let string_temp = reader.result.toString();
+                    let string_array = string_temp.split('/')
+
+                    if (props.actv_cat['img_cat'] != string_array) {
+                        //Setting data in firebase
+                        set(ref(db, 'cats_data/' + name + '/img_cat/'), string_array);
+                    }
+                    props.setActvCat({
+                        'name': name,
+                        'click': click,
+                        'img_cat': string_array,
+                    })
+                }
+                reader.readAsDataURL(img_cat);
+            } catch { }
+        }
+        else {
+            let click = document.getElementById('cat_click_input_modal').value;
+            let img_cat = document.getElementById('cat_img_input_modal').files[0];
+            try{
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                let string_temp = reader.result.toString(); 
+                let string_array = string_temp.split('/')
+                //Setting data in firebase
+                set(ref(db, 'cats_data/' + name + '/'), {
+                    'name': name,
+                    'click': click,
+                    'img_cat': string_array,
+                });
+                props.setActvCat({
+                    'name': name,
+                    'click': click,
+                    'img_cat': string_array,
+                })
+            };
+            reader.readAsDataURL(img_cat);
+        }catch{}
+    }
     }
 
 
@@ -79,16 +203,16 @@ const Form = (props) => {
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
-                            <div style={{'display':'flex','flexDirection':'column','rowGap':'15px'}}>
-                            <input style={{ 'paddingLeft': '15px' }} type="text" placeholder='Cat Name'
-                                id='cat_name_input' defaultValue={props.actv_cat['name']} />
-                            <input style={{ 'paddingLeft': '15px' }} defaultValue={props.actv_cat['click']} type="number" placeholder='Cat Clicks' id='cat_click_input' />
-                            <input type='file' id='cat_img_input' />
+                            <div style={{ 'display': 'flex', 'flexDirection': 'column', 'rowGap': '15px' }}>
+                                <input style={{ 'paddingLeft': '15px' }} type="text" placeholder='Cat Name'
+                                    id='cat_name_input_modal' defaultValue={props.actv_cat['name']} />
+                                <input style={{ 'paddingLeft': '15px' }} defaultValue={props.actv_cat['click']} type="number" placeholder='Cat Clicks' id='cat_click_input_modal' />
+                                <input type='file' id='cat_img_input_modal' />
                             </div>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Discard</button>
-                            <button type="button" className="btn btn-primary">Save Changes</button>
+                            <button type="button" className="btn btn-primary" onClick={(e) => modaleditFunction(e)}>Save Changes</button>
                         </div>
                     </div>
                 </div>
