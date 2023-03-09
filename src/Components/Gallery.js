f8cfb8d06222b4feb7d86d7f3f59d635bbeb491a
Mohit Sharma <mohit.vsht@gmail.com>
2023-03-09 17:31:35 +0530

    below section responsiveness done

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import db from '../firebase/firebase';
 import { ref, onValue } from 'firebase/database'
 import './static/css/gallery.css'
-import {clickupdate, ageValue} from './utility'
+import { clickupdate, ageValue } from './utility'
 
 const Gallery = (props) => {
     const [gallery_array, setGallery] = useState([]);
@@ -31,52 +31,54 @@ const Gallery = (props) => {
     }, []);
 
     // Update Clicks and Cat
-    function updateClicks(e){
-        let targ_name=e.target.id;
-        gallery_array.forEach(elem=>{
-            elem.map(value=>{
-                if(value['name']==targ_name){
-                    clickupdate(targ_name,value['click']);
+    function updateClicks(e) {
+        let targ_name = e.target.id;
+        gallery_array.forEach(elem => {
+            elem.map(value => {
+                if (value['name'] == targ_name) {
+                    clickupdate(targ_name, value['click']);
                     props.setActvCat({
-                        'name':targ_name,
-                        'click':parseInt(value['click'])+1,
-                        'img_cat':value['img_cat']
+                        'name': targ_name,
+                        'click': parseInt(value['click']) + 1,
+                        'img_cat': value['img_cat']
                     })
                 }
             })
         })
-        document.documentElement.scrollTop=0;
+        document.documentElement.scrollTop = 0;
     }
 
     return (
         <div>
             <div className='heading_gallery'>~ Cats Image Gallery ~</div>
-            <hr style={{'border':'1px','height':'2px','backgroundColor':'black'}} />
+            <hr style={{ 'border': '1px', 'height': '2px', 'backgroundColor': 'black' }} />
             <div className="gallery_holder">
                 {
-                    gallery_array.map(elem => { 
+                    gallery_array.map(elem => {
                         return (
-                            <div className="gallery_holder_flex" key={elem}>
-                                {
-                                    elem.map((value, index) => {
-                                        let img_url;
-                                        let data_array = value['img_cat'];
-                                        if (data_array) {
-                                            let data_string = data_array.join('/');
-                                            img_url = data_string;
-                                        }
-                                        return (
-                                            <div className="cat_gallry" key={value['name']}>
-                                                <div className="info_name_cat">{value['name']}</div>
-                                                <div className="gallery_img_holder">
-                                                    <img src={img_url} alt="" id={value['name']} onClick={updateClicks}/>
+                            <div className="respn_holder_gallery">
+                                <div className="gallery_holder_flex" key={elem}>
+                                    {
+                                        elem.map((value, index) => {
+                                            let img_url;
+                                            let data_array = value['img_cat'];
+                                            if (data_array) {
+                                                let data_string = data_array.join('/');
+                                                img_url = data_string;
+                                            }
+                                            return (
+                                                <div className="cat_gallry" key={value['name']}>
+                                                    <div className="info_name_cat_gallery">{value['name']}</div>
+                                                    <div className="gallery_img_holder">
+                                                        <img src={img_url} alt="" id={value['name']} onClick={updateClicks} />
+                                                    </div>
+                                                    <div className="cat_age_gallery"><strong>Current Age : </strong>{ageValue(parseInt(value['click']))}</div>
+                                                    <div className="cat_click_no_gallery"><strong>No of Clicks : </strong>{value['click']}</div>
                                                 </div>
-                                                <div className="cat_age"><strong>Current Age : </strong>{ageValue(parseInt(value['click']))}</div>
-                                                <div className="cat_click_no"><strong>No of Clicks : </strong>{value['click']}</div>
-                                            </div>
-                                        )
-                                    })
-                                }
+                                            )
+                                        })
+                                    }
+                                </div>
                             </div>
                         )
                     })
